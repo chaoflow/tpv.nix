a805e150923066fb071826cbef10c36d57f6c029
Florian Friesdorf <flo@chaoflow.net>
2013-07-27 17:40:08 +0200

    xin generation taking form

--- src/tpv/nix/xin/generation.py
+++ src/tpv/nix/xin/generation.py
@@ -1,17 +1,45 @@
+import os
 import tpv.cli
 
-from tpv.ordereddict import OrderedDict
+from plumbum import FG
+from plumbum.cmd import nix_env
 
 
-class Generation(OrderedDict):
+class Generation(tpv.cli.Command):
     """Manage generations of a profile
     """
+    profile = tpv.cli.SwitchAttr(
+        ['-p', '--profile'], str,
+        help="Name of profile within " + os.environ['NIX_USER_PROFILE_DIR'],
+    )
+
+    @property
+    def profile_path(self):
+        return os.path.sep.join([os.environ['NIX_USER_PROFILE_DIR'],
+                                 self.profile])
+
     def __call__(self):
-        print "huhu"
+        cmd = nix_env
+        if self.profile:
+            cmd = cmd[['-p', self.profile]]
+        cmd['--list-generations'] & FG
 
 
-class Remove(OrderedDict):
+class Remove(tpv.cli.Command):
     """Remove generations
     """
-    def __call__(self):
-        print "huhu"
+    profile = tpv.cli.SwitchAttr(
+        ['-p', '--profile'], str,
+        help="Name of profile within " + os.environ['NIX_USER_PROFILE_DIR'],
+    )
+
+    @property
+    def profile_path(self):
+        return os.path.sep.join([os.environ['NIX_USER_PROFILE_DIR'],
+                                 self.profile])
+
+    def __call__(self, *generations):
+        cmd = nix_env
+        if self.profile:
+            cmd = cmd[['-p', self.profile]]
+        nix_env[['--delete-generations'] + list(generations)] & FG
